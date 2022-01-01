dc02de6151edd55afea115fdd4d81699efafc1f9
Jens Jäger <jaegerj204@googlemail.com>
2022-01-01 12:12:06 +0100

    Some basic recipts

---
+++ items.ts
@@ -0,0 +1,23 @@
+export enum Item {
+    IronOre,
+    IronIngot,
+    IronPlate,
+    IronRod,
+
+    Coal,
+    CompactedCoal,
+    PetroleumCoke,
+    
+    SteelIngot,
+    SteelBeam,
+    SteelPipe,
+
+    CopperOre,
+    CopperIngot,
+    Wire,
+    Cable,
+
+    Water,
+
+    Plastic,
+}
