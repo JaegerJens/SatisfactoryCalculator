2b455794a0c8e87bf8a30f9b8597a487035376ba
Jens Jäger <jaegerj204@googlemail.com>
2022-01-07 08:42:27 +0100

    Plan new steel factory from 1 iron source

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
+import { Item } from "./items.ts";
 import { planFactory } from "./plan-factory.ts";
 import { Production } from "./production.ts";
-import { avoidScrews, book } from "./recipes/index.ts";
+import { PureIronIngot } from "./recipes/iron-ingot.ts";
+import { SolidSteelIngot } from "./recipes/steel-ingot.ts";
+import { SelectRecipe } from "./types.ts";
 
-const target = new Production(book.ModularFrameRecipes[0], 4);
+const target = new Production(SolidSteelIngot, 36);
 
-const factory = planFactory(target, avoidScrews);
+const steelFactory: SelectRecipe = item => {
+    switch (item) {
+        case Item.SteelIngot: return SolidSteelIngot;
+        case Item.IronIngot: return PureIronIngot;
+    }
+
+}
+
+const factory = planFactory(target, steelFactory);
 
 console.log(factory.map(prod => prod.toString()).join('\n'));
