ac5c00a90cf3787f0a35eca7918512533794654f
Jens Jäger <jaegerj204@googlemail.com>
2022-01-08 13:33:05 +0100

    Fix ratio of uranium to plutonium power

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
+import { formatThroughput } from "./format.ts";
 import { planFactory } from "./plan-factory.ts";
 import { Production } from "./production.ts";
 import { useAnyRecipe } from "./recipes/index.ts";
 import { PlutoniumPower, UraniumPower } from "./recipes/nuclear-power.ts";
 
-const uraniumPower = new Production(UraniumPower, 10);
+console.log('## URANIUM POWER');
+const uraniumPower = new Production(UraniumPower, 5);
 const uraniumPowerPlants = planFactory([uraniumPower], useAnyRecipe);
 console.log(uraniumPowerPlants.map(prod => prod.toString()).join('\n'));
+console.log('OUTPUT: ', uraniumPower.output(60).map(formatThroughput).join(','));
 
-const plutoniumPower = new Production(PlutoniumPower, 1);
+console.log('\n\n## PLUTONIUM POWER');
+const plutoniumPower = new Production(PlutoniumPower, 10);
 const plutoniumPowerPlants = planFactory([plutoniumPower], useAnyRecipe);
 console.log(plutoniumPowerPlants.map(prod => prod.toString()).join('\n'));
+console.log('OUTPUT: ', plutoniumPower.output(60).map(formatThroughput).join(','));
