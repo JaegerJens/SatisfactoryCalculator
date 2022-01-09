28d83abe5d21943a006f3d6089e44e0550fc6fbf
Jens Jäger <jaegerj204@googlemail.com>
2022-01-09 09:30:33 +0100

    Sink nuclear waste with plutonium fuel rods

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import { formatThroughput } from "./format.ts";
 import { planFactory } from "./plan-factory.ts";
 import { Production } from "./production.ts";
 import { useAnyRecipe } from "./recipes/index.ts";
-import { PlutoniumPower, UraniumPower } from "./recipes/nuclear-power.ts";
+import { UraniumPower } from "./recipes/nuclear-power.ts";
+import { PlutoniumFuelRod } from "./recipes/plutonium-fuel-rod.ts";
 
 console.log('## URANIUM POWER');
 const uraniumPower = new Production(UraniumPower, 5);
@@ -10,8 +11,8 @@ const uraniumPowerPlants = planFactory([uraniumPower], useAnyRecipe);
 console.log(uraniumPowerPlants.map(prod => prod.toString()).join('\n'));
 console.log('OUTPUT: ', uraniumPower.output(60).map(formatThroughput).join(','));
 
-console.log('\n\n## PLUTONIUM POWER');
-const plutoniumPower = new Production(PlutoniumPower, 10);
-const plutoniumPowerPlants = planFactory([plutoniumPower], useAnyRecipe);
-console.log(plutoniumPowerPlants.map(prod => prod.toString()).join('\n'));
-console.log('OUTPUT: ', plutoniumPower.output(60).map(formatThroughput).join(','));
+console.log('\n\n## PLUTONIUM FUEL RODS SINKING');
+const plutoniumFuelRods = new Production(PlutoniumFuelRod, 4);
+const sinkWasteFactory = planFactory([plutoniumFuelRods], useAnyRecipe);
+console.log(sinkWasteFactory.map(prod => prod.toString()).join('\n'));
+console.log('OUTPUT: ', plutoniumFuelRods.output(60).map(formatThroughput).join(','));
