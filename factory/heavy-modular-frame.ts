1a53c037b6364718addddacce9db03472de66dca
Jens Jäger <jaegerj204@googlemail.com>
2023-11-26 09:38:09 +0100

    use only steel pipes

--- factory/heavy-modular-frame.ts
+++ factory/heavy-modular-frame.ts
@@ -2,6 +2,7 @@ import { Building } from "../buildings.ts";
 import { Item } from "../items.ts";
 import { planFactory } from "../plan-factory.ts";
 import { Production } from "../production.ts";
+import { EncasedIndustrialPipe } from "../recipes/encased-industrial-beam.ts";
 import { HeavyEncasedFrame } from "../recipes/heavy-modular-frame.ts";
 import { useBasicRecipes } from "../recipes/index.ts";
 import { PureIronIngot } from "../recipes/iron-ingot.ts";
@@ -19,11 +20,10 @@ const heavyModularFrameRecipes: SelectRecipe = item => {
     switch(item) {
         case Item.ModularFrame: return SteeledFrame;
         case Item.ReinforcedIronPlate: return StitchedIronPlate;
+        case Item.EncasedIndustrialBeam: return EncasedIndustrialPipe;
         case Item.Wire: return IronWire;
         case Item.SteelIngot: return SolidSteelIngot;
         case Item.IronIngot: return PureIronIngot;
-        case Item.CateriumOre:
-            return source(item, Building.MinerMk3, Purity.Normal);
         case Item.IronOre: return source(item, Building.MinerMk3, Purity.Normal);
         case Item.Coal: return source(item, Building.MinerMk3, Purity.Normal);
         case Item.Limestone: return source(item, Building.MinerMk3, Purity.Normal);
